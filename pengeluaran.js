1639ee11ed6ede20a5a06b3ddd1a32be2dc34e1f
Amindoant <122103624+Amindoant@users.noreply.github.com>
2026-05-29 06:14:20 +0700

    ubah

--- pengeluaran.js
+++ pengeluaran.js
@@ -20,6 +20,14 @@ async function loadPengeluaran() {
     const tanggal = item.tanggal;
     const keterangan = item.keterangan;
 
+    let nota = item.nota || "";
+
+    // otomatis convert link imgur biasa
+    if(nota.includes("imgur.com") && !nota.includes("i.imgur.com")){
+        const id = nota.split("/").pop();
+        nota = `https://i.imgur.com/${id}.jpg`;
+    }
+
     const jumlah = Number(
       (item.jumlah || "0")
       .toString()
@@ -28,10 +36,30 @@ async function loadPengeluaran() {
 
     tbody.innerHTML += `
       <tr>
+
         <td>${no++}</td>
+
         <td>${tanggal}</td>
+
         <td>${keterangan}</td>
-        <td>Rp ${jumlah.toLocaleString()}</td>
+
+        <td>Rp ${jumlah.toLocaleString('id-ID')}</td>
+
+        <td>
+          ${
+            nota
+            ? `
+              <img
+                src="${nota}"
+                alt="Nota"
+                class="nota-img"
+                onclick="showNota('${nota}')"
+              >
+            `
+            : '-'
+          }
+        </td>
+
       </tr>
     `;
 
@@ -40,7 +68,27 @@ async function loadPengeluaran() {
   });
 
   document.getElementById("totalPengeluaran")
-    .innerText = total.toLocaleString();
+    .innerText = total.toLocaleString('id-ID');
+}
+
+
+function showNota(url){
+
+  const modal = document.createElement('div');
+
+  modal.className = "modal-nota";
+
+  modal.innerHTML = `
+      <span class="close-btn">&times;</span>
+      <img src="${url}" class="modal-image">
+  `;
+
+  document.body.appendChild(modal);
+
+  modal.addEventListener('click', () => {
+      modal.remove();
+  });
+
 }
 
 loadPengeluaran();
